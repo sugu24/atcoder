aaa46755fc8e8aaa84c5fb87928f7ff56ee3037e
sugu24 <shayasugu@icloud.com>
2021-09-13 16:09:10 +0900

    lazyseg

---
+++ lazysegment.py
@@ -0,0 +1,94 @@
+def segfunc(x,y):
+    return max(x,y)
+
+ide_ele = 0
+
+class LazySegmentTree:
+    # index 0 は無視
+    def __init__(self, init_val, segfunc, ide_ele) -> None:
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.data = [ide_ele] * 2 * self.num
+        self.lazy = [None] * 2 * self.num
+        for i in range(n):
+            self.data[self.num + i] = init_val[i]
+        for i in range(self.num - 1, 0, -1):
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
+        
+    # (l,r] 伝搬する必要があるところだけとる
+    # 伝搬する必要がないとは更新区間と被ること
+    def gindex(self, l, r):
+        l += self.num
+        r += self.num
+        lm = l >> (l & -l).bit_length()
+        rm = r >> (r & -r).bit_length()
+        while l < r:
+            if l <= lm:
+                yield l
+            if r <= rm:
+                yield r
+            r >>= 1
+            l >>= 1
+        while l:
+            yield l
+            l >>= 1
+
+    # 子ノードに伝搬する
+    def propagates(self, *ids):
+        for i in reversed(ids):
+            v = self.lazy[i]
+            if v is None:
+                continue
+            self.lazy[2 * i] = v
+            self.lazy[2 * i + 1] = v
+            self.data[2 * i] = v
+            self.data[2 * i + 1] = v
+            self.lazy[i] = None
+
+
+    # (l,r]
+    def update(self, l, r, x):
+        *ids, = self.gindex(l, r)
+        self.propagates(*ids)
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                self.lazy[l] = x
+                self.data[l] = x
+                l += 1
+            if r & 1:
+                self.lazy[r - 1] = x
+                self.data[r - 1] = x
+            r >>= 1
+            l >>= 1
+        for i in ids:
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
+
+    #(l,r]
+    def query(self, l, r):
+        *ids, = self.gindex(l, r)
+        self.propagates(*ids)
+        res = self.ide_ele
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.data[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.data[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+
+
+W,N = map(int,input().split())
+seg = LazySegmentTree([0]*(W+1), segfunc, ide_ele)
+for _ in range(N):
+    l,r = map(int,input().split())
+    height = seg.query(l,r+1) + 1
+    seg.update(l, r+1, height)
+    print(height)
